01e659247e12f6acb5b92046cd77929ddda2fb75
Bears <evanbero1980@gmail.com>
2020-10-04 10:10:01 -0400

    cleaned up code, final push

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-// Require Dependencies
+// Require Dependencies [express, file system, path]
 const express = require("express");
 const fs = require("fs");
 const path = require('path');
 
-// Initialize express app
+// Initialize express[to run on port 3000]
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Setup data parsing
+//Data parse
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
 
-//Require routes file
+//Require routes
 require('./routes/routes')(app);
 
-// Setup listener
+// Listener
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
 });  
